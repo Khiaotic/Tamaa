f6acdcc8fdbd89a5a93a6a159dab9448117ac7df
Khiaotic <112679225+Khiaotic@users.noreply.github.com>
2024-06-14 14:41:05 -0500

    content clickable and linkable!

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Section from "./Section";
 import Heading from "./Heading";
 import { IndexContent } from "../constants/IndexContent";
+// import ytIcon from "../assets/benefits/yt_icon.svg"
 
 
 const Content = () => {
@@ -22,17 +23,24 @@ const Content = () => {
              }}
             key={item.id}>
               
-              <div className="relative z-2 flex flex-col min-h-[22rem] p=[2
-              .4rem] pointer-events-none">
+              <div className="relative z-2 flex flex-col min-h-[22rem] p-6 pointer-events-none">
                 <h5 className="h2 text-center mb-5">{item.title}</h5>
                 <p className="body-2 mb-6 text-n-3">{item.text}</p>
-                <div className="flex items-center mt-auto">
-<img 
-src={item.iconUrl}
-width={48}
-height={48}
-alt={item.title}
-/>
+                <div>
+                  
+                    <img
+                    src={item.thumbnail}
+                    className="w-full h-auto"
+                    width={800}
+                    height={300}
+                     />
+                  
+                </div>
+                <div className="flex pl-6 items-center mt-auto">
+               <svg id="a" xmlns="http://www.w3.org/2000/svg" width={48} height={48} viewBox="0 0 192 192">
+               <path d="M180.32,53.36c-2.02-7.62-7.99-13.62-15.56-15.66-13.73-3.7-68.76-3.7-68.76-3.7,0,0-55.04,0-68.76,3.7-7.57,2.04-13.54,8.04-15.56,15.66-3.68,13.81-3.68,42.64-3.68,42.64,0,0,0,28.82,3.68,42.64,2.02,7.62,7.99,13.62,15.56,15.66,13.73,3.7,68.76,3.7,68.76,3.7,0,0,55.04,0,68.76-3.7,7.57-2.04,13.54-8.04,15.56-15.66,3.68-13.81,3.68-42.64,3.68-42.64,0,0,0-28.82-3.68-42.64Z" fill="red" strokeWidth="0"/>
+               <polygon points="78 122.17 124 96 78 69.83 78 122.17" fill="#fff" strokeWidth="0"/>
+               </svg>
 
                 </div>
               </div>
--- src/constants/IndexContent.js
+++ src/constants/IndexContent.js
@@ -1,20 +1,26 @@
-import React from "react";
-// import {dream, mine, down, fight, fire,} from "../assets"
-import {youtubeIcon} from "../assets"
+
+
+// import {ytIcon} from "../assets/benefits/yt_icon.svg"
+
+
+
+
 export const IndexContent = [
   {
     id: "0",
     title: "Going Down",
     text: "When you come over, it's going down",
     backgroundUrl: "./src/assets/benefits/card-1.svg",
-  iconUrl: youtubeIcon
+    thumbnail:"../assets/benefits/down.jpg",
+  // iconUrl: ytIcon,
   },
   {
     id: "1",
     title: "Time with You",
     text: "The app uses natural language processing to understand user queries and provide accurate and relevant responses.",
     backgroundUrl: "./src/assets/benefits/card-6.svg",
-    iconUrl: youtubeIcon,
+    thumbnail:"../assets/benefits/fight.jpg",
+    // iconUrl: ytIcon,
     light: true,
     },
     {
@@ -22,14 +28,16 @@ export const IndexContent = [
       title: "TAMA x FIRE BREATHING",
       text: "Why did you come in my space with your toxic ass energy, FUCK",
       backgroundUrl: "./src/assets/benefits/card-3.svg",
-      iconUrl:youtubeIcon,
+      thumbnail:"../assets/benefits/fire.jpg",
+      // iconUrl:ytIcon,
       },
       {
         id: "3",
         title: "It's Mine",
         text: "You can't take it from me, ITS MINE",
         backgroundUrl: "./src/assets/benefits/card-3.svg",
-        iconUrl:youtubeIcon,
+        thumbnail:"../assets/benefits/mine.jpg",
+        // iconUrl:ytIcon,
         
         light: true,
         },
@@ -38,13 +46,15 @@ export const IndexContent = [
           title: "TAMA x FIGHT ME",
           text: "Always Talkin' shit but you LOVE me",
           backgroundUrl: "./src/assets/benefits/card-6.svg",
-        iconUrl:youtubeIcon,
-        },
-        {
-          id: "4",
-          title: "Dream Girl",
-          text: "When I close my eyes I ge to see your face ",
-          backgroundUrl: "./src/assets/benefits/card-1.svg",
-          iconUrl:youtubeIcon,
+          thumbnail:"../assets/benefits/fire.jpg",
+          // iconUrl:ytIcon,
+          },
+          {
+            id: "4",
+            title: "Dream Girl",
+            text: "When I close my eyes I ge to see your face ",
+            backgroundUrl: "./src/assets/benefits/card-1.svg",
+            thumbnail:"../assets/benefits/dream.jpg",
+          // iconUrl:ytIcon,
   },
 ];
